ba7cb5b994c921e87fdf7bf61db6b70878db7771
Terer <terermike3@gmail.com>
2024-04-08 19:36:00 +0300

    Copleted the demo ReacrArt app using Tailwind CSS

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -4,13 +4,6 @@ import { styled } from "styled-components";
 import Button from "./Button";
 import Input from "./Input";
 
-const ControlContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  margin-bottom: 1.5rem;
-`;
-
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState("");
   const [enteredPassword, setEnteredPassword] = useState("");
@@ -32,12 +25,15 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <ControlContainer>
+    <div
+      id="auth-inputs"
+      className="w-full max-w-sm p-8 mx-auto rounded shadow-md bg-gradient-to-b from-stone-700 to-stone-800"
+    >
+      <div className="flex flex-col gap-2 mb-6">
         <Input
           label="Email"
           type="email"
-          $invalid={emailNotValid}
+          invalid={emailNotValid}
           // className={emailNotValid ? "$invalid" : undefined}
           // style={{ backgroundColor: emailNotValid ? "#fed2d2" : "#d1d5db" }}
           onChange={(event) => handleInputChange("email", event.target.value)}
@@ -46,15 +42,15 @@ export default function AuthInputs() {
         <Input
           label="Password"
           type="password"
-          $invalid={passwordNotValid}
+          invalid={passwordNotValid}
           // className={passwordNotValid ? "$invalid" : undefined}
           onChange={(event) =>
             handleInputChange("password", event.target.value)
           }
         />
-      </ControlContainer>
-      <div className="actions">
-        <button type="button" className="text-button">
+      </div>
+      <div className="flex justify-end gap-4">
+        <button type="button" className="text-amber-400 hover:text-amber-500">
           Create a new account
         </button>
         <Button onClick={handleLogin}>Sign In</Button>
